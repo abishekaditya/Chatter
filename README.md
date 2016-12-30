d6ead7e9a4c731f1394a20090c4136884954a93d
Abishek Aditya <abishekaditya@outlook.com>
2016-12-31 04:07:52 +0530

    Basic Chat without auth

---
+++ README.md
@@ -0,0 +1,14 @@
+# README
+
+To Do :-
+
+* Add Devise Authentication
+
+* Add Colours to the UserName/Chats
+
+* Move all CSS into a Sass Template
+
+* UI Design
+
+* Heroku Config
+
---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,13 @@
+class RoomsController < ApplicationController
+
+  http_basic_authenticate_with name: "admin", password: ENV['pass'], except: [:show]
+  def show
+    @messages= Message.all
+  end
+
+  def delete
+    reset_session
+    Message.delete_all
+    redirect_to :back
+  end
+end
